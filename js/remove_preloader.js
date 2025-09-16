c0c4e1b3d3c55137536a30de56d38cb038ac58f0
Hitesh Shrivastava <hiteshssm476@gmail.com>
2025-09-16 16:21:17 +0530

    A short message about the changes you made

--- js/remove_preloader.js
+++ js/remove_preloader.js
@@ -3,5 +3,5 @@ window.addEventListener("load", function() {
   preloader.style.opacity = "0"; // start fade out
   setTimeout(() => {
     preloader.style.display = "none"; // remove after fade completes
-  }, 7000); // matches 7s transition
+  }, 1000); // matches 7s transition
 });
